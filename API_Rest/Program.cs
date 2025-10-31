a1529104402689f0e564680f9e0836c8a4fbce7a
alexeys6969 <alexshashin.2023@gmail.com>
2025-10-31 12:13:05 +0500

    Струтктура проекта.Дегтянников.

--- API_Rest/Program.cs
+++ API_Rest/Program.cs
@@ -1,25 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API системы прогресса",
+        Description = "—истема выполнени€ заданий и разблокировки изображений"
+    });
+    c.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = "API системы прогресса",
+        Description = "—истема выполнени€ заданий и разблокировки изображений"
+    });
+    c.SwaggerDoc("v3", new OpenApiInfo
+    {
+        Version = "v3",
+        Title = "API системы прогресса",
+        Description = "—истема выполнени€ заданий и разблокировки изображений"
+    });
+    c.SwaggerDoc("v4", new OpenApiInfo
+    {
+        Version = "v4",
+        Title = "API системы прогресса",
+        Description = "—истема выполнени€ заданий и разблокировки изображений"
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseStatusCodePages();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "«апросы GET");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "«апросы POST");
+        c.SwaggerEndpoint("/swagger/v3/swagger.json", "«апросы PUT");
+        c.SwaggerEndpoint("/swagger/v4/swagger.json", "«апросы DELETE");
+    });
+}
+else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
+app.MapControllers();
 app.Run();
