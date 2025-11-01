ed8114da74013f8fc544c14c807ab20de4f87e24
alexeys6969 <alexshashin.2023@gmail.com>
2025-11-01 11:35:09 +0500

    Авторизация регистрация.Kan

---
+++ GameKAn/Pages/ReginPol.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameKAn.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ReginPol.xaml
+    /// </summary>
+    public partial class ReginPol : Page
+    {
+        public ReginPol()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGoToLoginClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnRegisterClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
